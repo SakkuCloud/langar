101dddd431c03887ab351f61982c2a7c05757fbb
Seyedmajid Etehadi <ettehadi.m@gmail.com>
2019-11-27 12:55:55 +0330

    Bug fix in config log address

--- config/config.go
+++ config/config.go
@@ -5,12 +5,14 @@ import "time"
 const (
 	SentryTimeout time.Duration = 10
 
+	DockerTimeout time.Duration = 10
+
 	InvalidArgsMessage string = "invalid args"
 )
 
 var Config = struct {
 	Port    string `default:"3000"`
-	LogFile string `default:"/var/log/khazen.log"`
+	LogFile string `default:"/var/log/langar.log"`
 
 	SentryDSN string
 
